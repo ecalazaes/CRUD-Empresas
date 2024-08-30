d8cfae9bb5f1cd4e3b6cdd36afe242e364d3ea11
Erick Calazães <ecalazaes@gmail.com>
2024-08-30 14:44:24 -0300

    Projeto Crud Empresas

---
+++ src/main/java/rj/senac/br/TestAPI/controllers/EmpresaController.java
@@ -0,0 +1,65 @@
+package rj.senac.br.TestAPI.controllers;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rj.senac.br.TestAPI.entities.Empresa;
+import rj.senac.br.TestAPI.services.EmpresaService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping(value = "/empresas")
+@CrossOrigin(origins = "http://localhost:4200") // Permitir CORS para essa origem
+public class EmpresaController {
+
+    private EmpresaService empresaService;
+
+    public EmpresaController(EmpresaService empresaService) {
+        this.empresaService = empresaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Empresa>> getAllEmpresas() {
+        List<Empresa> list = empresaService.getAllEmpresas();
+        return list.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Empresa> getEmpresaById(@PathVariable Long id) {
+        try {
+            Empresa empresa = empresaService.getEmpresaById(id);
+            return ResponseEntity.ok(empresa);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Empresa> insertEmpresa(@Valid @RequestBody Empresa empresa) {
+        Empresa createdEmpresa = empresaService.insertEmpresa(empresa);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdEmpresa);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Void> deleteEmpresa(@PathVariable Long id) {
+        try {
+            empresaService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @PutMapping(value = "update/{id}")
+    public ResponseEntity<Empresa> updateEmpresa(@PathVariable Long id, @Valid @RequestBody Empresa empresa) {
+        try {
+            empresa = empresaService.updateEmpresa(id, empresa);
+            return ResponseEntity.ok(empresa);
+        } catch (EntityNotFoundException e ) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
